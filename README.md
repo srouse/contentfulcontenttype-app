2b78c04019d74ef3f9a6b49076a658fae3350ec4
Scott Rouse <scott.rouse@gmail.com>
2023-03-05 16:00:16 -0600

    adding images and integrating dsys

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
   Content details in Build Information below.
 -->
 
+- [Description](#description)
+- [Contentful Content Type Variations](#contentful-content-type-variations)
+- [Editing Proposed Content Types](#editing-proposed-content-types)
+- [Loading Content Types from API](#loading-content-types-from-api)
+- [Generating Examples](#generating-examples)
+- [Full Example of Content Type Modeling](#full-example-of-content-type-modeling)
 - [Build Information](#build-information)
 
 ---
@@ -19,13 +25,57 @@ __Figma Widget URL__: https://www.figma.com/community/widget/1138479828934967837
 
 __Production URL__: https://contentfulcontenttype.app
 
+## Description
+
+The complete Contentful Content Type visualization tool ( https://www.contentful.com/ ) for Content Types and Entries. For new content types, create a proposed content type using the property types available in Contentful. For existing content types, input your space id, environment, and access token and choose to visualize an existing content type. 
+
+When you have a Content Type, click the "+" in the header to create an Entry of that Content Type.
+
+## Contentful Content Type Variations
+
+[![Contentful Content Type Variations](https://images.ctfassets.net/rtkhko6y3s3u/2ACZNvb0tl5ZgV084zDhuv/f0efed997de544981757c4b7e541f391/Contentful_Content_Type_Variations.png)](https://images.ctfassets.net/rtkhko6y3s3u/2ACZNvb0tl5ZgV084zDhuv/f0efed997de544981757c4b7e541f391/Contentful_Content_Type_Variations.png "View Full Size")
+    
+Contentful Content Type Variations [source](https://www.figma.com/file/u1sVmfRm0IWGj2KXlzlN5l/?node-id=3:82)
+
+
+## Editing Proposed Content Types
+
+[![Editing Proposed Content Types](https://images.ctfassets.net/rtkhko6y3s3u/5mA6cawyfyZM74OA4BJlg3/73dd181d9a4ed65ecb459f783d49701f/Editing_Proposed_Content_Types.png)](https://images.ctfassets.net/rtkhko6y3s3u/5mA6cawyfyZM74OA4BJlg3/73dd181d9a4ed65ecb459f783d49701f/Editing_Proposed_Content_Types.png "View Full Size")
+    
+Editing Proposed Content Types [source](https://www.figma.com/file/u1sVmfRm0IWGj2KXlzlN5l/?node-id=6:37)
+
+
+## Loading Content Types from API
+
+[![Loading Content Types from API](https://images.ctfassets.net/rtkhko6y3s3u/1v8Ul5Mhw6FEFohJ2JA3WS/78da76c883de5e83d022ead90a10f0d8/Loading_Content_Types_from_API.png)](https://images.ctfassets.net/rtkhko6y3s3u/1v8Ul5Mhw6FEFohJ2JA3WS/78da76c883de5e83d022ead90a10f0d8/Loading_Content_Types_from_API.png "View Full Size")
+    
+Loading Content Types from API [source](https://www.figma.com/file/u1sVmfRm0IWGj2KXlzlN5l/?node-id=7:42)
+
+
+## Generating Examples
+
+[![Generating Examples](https://images.ctfassets.net/rtkhko6y3s3u/7LFsLxfL7Ar9svYyeUd569/97034994828bc9835d93727f9169f4b5/Generating_Examples.png)](https://images.ctfassets.net/rtkhko6y3s3u/7LFsLxfL7Ar9svYyeUd569/97034994828bc9835d93727f9169f4b5/Generating_Examples.png "View Full Size")
+    
+Generating Examples [source](https://www.figma.com/file/u1sVmfRm0IWGj2KXlzlN5l/?node-id=8:428)
+
+
+## Full Example of Content Type Modeling
+This is an example of a full modeling visual in progress. The canvas starts with mostly proposed Content Types and one by one makes it's way into migrating into Content Types loaded from the API when they are made. 
+
+Each Content Type can spawn an example like the two blue widgets on the right. They are a way of practicing how the Content Type will grow and present itself with a user. It is sometimes difficult to imagine how the Content Type is interpreted to a user who usually has an idea of the content they are trying to add. For instance, the example shows how a user could take two different paths for creating a piece of content.
+
+[![Full Example of Content Type Modeling](https://images.ctfassets.net/rtkhko6y3s3u/5nXIG7hlSBP4DffK6VrauK/5d7ca8152050fabdcd9fd0a35dbfcc5f/Full_Example_of_Content_Type_Modeling.png)](https://images.ctfassets.net/rtkhko6y3s3u/5nXIG7hlSBP4DffK6VrauK/5d7ca8152050fabdcd9fd0a35dbfcc5f/Full_Example_of_Content_Type_Modeling.png "View Full Size")
+    
+Full Example of Content Type Modeling [source](https://www.figma.com/file/u1sVmfRm0IWGj2KXlzlN5l/?node-id=8:1779)
+
+
 ## Build Information
 
 *Dynamically built using contentful-readme-generator. Do not edit directly.*
 
-*__updated__: 3/4/2023, 3:13:58 PM*
+*__updated__: 3/5/2023, 3:27:41 PM*
 
-*__built__: 3/4/2023, 3:14:50 PM*
+*__built__: 3/5/2023, 3:58:17 PM*
 
 *__space__: rtkhko6y3s3u*
 
--- templates/htmlTemplate.mjs
+++ templates/htmlTemplate.mjs
@@ -5,24 +5,33 @@ export default function renderHTML(content, css) {
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="https://admirable-pegasus-a42d6b.netlify.app/transformations/fdst-web/rds.css" />
     <style>
       ${css}
     </style>
     <style>
-      :root {
-        --white-color: #fff;
-
-        --primary-color: #A164F9;
-        --primary-dark-color: #7014F6;
-      }
 
       html, body {
+        font-size: 16px;
         margin: 0; padding: 0;
+        background-color: var( --rds-color-primary-5 );
+        color: var( --rds-color-grey-2 );
+      }
+
+      .markdown-body {
+        font: var( --rds-type-text );
+      }
+
+      .markdown-body h2 {
+        font: var( --rds-type-text-8 );
+        margin-top: calc( var( --rds-spacing-2 ) + var( --rds-spacing-0-6 ));
+        margin-bottom: var( --rds-spacing-1 );
+        padding: 0;
       }
 
       .header {
-        background-color: var( --primary-dark-color );
-        color: var( --white-color );
+        background-color: var( --color-header );
+        color: var( --color-header-text );
         font-size: 1.4em;
         height: 140px;
         display: flex;
@@ -74,6 +83,111 @@ export default function renderHTML(content, css) {
         .markdown-content {
           padding-top: 20px;
           padding-bottom: 30px;
+        }
+      }
+
+
+      @media (prefers-color-scheme: dark) {
+        .markdown-body {
+          color-scheme: dark;
+          --color-prettylights-syntax-comment: #8b949e;
+          --color-prettylights-syntax-constant: #79c0ff;
+          --color-prettylights-syntax-entity: #d2a8ff;
+          --color-prettylights-syntax-storage-modifier-import: #c9d1d9;
+          --color-prettylights-syntax-entity-tag: #7ee787;
+          --color-prettylights-syntax-keyword: #ff7b72;
+          --color-prettylights-syntax-string: #a5d6ff;
+          --color-prettylights-syntax-variable: #ffa657;
+          --color-prettylights-syntax-brackethighlighter-unmatched: #f85149;
+          --color-prettylights-syntax-invalid-illegal-text: #f0f6fc;
+          --color-prettylights-syntax-invalid-illegal-bg: #8e1519;
+          --color-prettylights-syntax-carriage-return-text: #f0f6fc;
+          --color-prettylights-syntax-carriage-return-bg: #b62324;
+          --color-prettylights-syntax-string-regexp: #7ee787;
+          --color-prettylights-syntax-markup-list: #f2cc60;
+          --color-prettylights-syntax-markup-heading: #1f6feb;
+          --color-prettylights-syntax-markup-italic: #c9d1d9;
+          --color-prettylights-syntax-markup-bold: #c9d1d9;
+          --color-prettylights-syntax-markup-deleted-text: #ffdcd7;
+          --color-prettylights-syntax-markup-deleted-bg: #67060c;
+          --color-prettylights-syntax-markup-inserted-text: #aff5b4;
+          --color-prettylights-syntax-markup-inserted-bg: #033a16;
+          --color-prettylights-syntax-markup-changed-text: #ffdfb6;
+          --color-prettylights-syntax-markup-changed-bg: #5a1e02;
+          --color-prettylights-syntax-markup-ignored-text: #c9d1d9;
+          --color-prettylights-syntax-markup-ignored-bg: #1158c7;
+          --color-prettylights-syntax-meta-diff-range: #d2a8ff;
+          --color-prettylights-syntax-brackethighlighter-angle: #8b949e;
+          --color-prettylights-syntax-sublimelinter-gutter-mark: #484f58;
+          --color-prettylights-syntax-constant-other-reference-link: #a5d6ff;
+
+          --color-fg-default: var( --rds-color-grey-9 );
+          --color-fg-muted: var( --rds-color-grey-8 );
+          --color-fg-subtle: var( --rds-color-grey-7 );
+          --color-canvas-default: var( --rds-color-grey-0 );
+          --color-canvas-subtle: var( --rds-color-grey-1 );
+          --color-border-default: var( --rds-color-grey-2 );
+          --color-border-muted: var( --rds-color-grey-4 );
+          --color-neutral-muted: var( --rds-color-grey-5 );
+          --color-accent-fg: var( --rds-color-primary-5 );
+          --color-accent-emphasis: var( --rds-color-primary-5 );
+          --color-attention-subtle: var( --rds-color-primary-1 );
+          --color-danger-fg: var( --rds-color-primary-1 );
+
+          --color-header: var( --rds-color-primary-5 );
+          --color-header-text: var( --rds-color-grey-10 );
+        }
+      }
+      
+      @media (prefers-color-scheme: light) {
+        .markdown-body {
+          color-scheme: light;
+          --color-prettylights-syntax-comment: #6e7781;
+          --color-prettylights-syntax-constant: #0550ae;
+          --color-prettylights-syntax-entity: #8250df;
+          --color-prettylights-syntax-storage-modifier-import: #24292f;
+          --color-prettylights-syntax-entity-tag: #116329;
+          --color-prettylights-syntax-keyword: #cf222e;
+          --color-prettylights-syntax-string: #0a3069;
+          --color-prettylights-syntax-variable: #953800;
+          --color-prettylights-syntax-brackethighlighter-unmatched: #82071e;
+          --color-prettylights-syntax-invalid-illegal-text: #f6f8fa;
+          --color-prettylights-syntax-invalid-illegal-bg: #82071e;
+          --color-prettylights-syntax-carriage-return-text: #f6f8fa;
+          --color-prettylights-syntax-carriage-return-bg: #cf222e;
+          --color-prettylights-syntax-string-regexp: #116329;
+          --color-prettylights-syntax-markup-list: #3b2300;
+          --color-prettylights-syntax-markup-heading: #0550ae;
+          --color-prettylights-syntax-markup-italic: #24292f;
+          --color-prettylights-syntax-markup-bold: #24292f;
+          --color-prettylights-syntax-markup-deleted-text: #82071e;
+          --color-prettylights-syntax-markup-deleted-bg: #ffebe9;
+          --color-prettylights-syntax-markup-inserted-text: #116329;
+          --color-prettylights-syntax-markup-inserted-bg: #dafbe1;
+          --color-prettylights-syntax-markup-changed-text: #953800;
+          --color-prettylights-syntax-markup-changed-bg: #ffd8b5;
+          --color-prettylights-syntax-markup-ignored-text: #eaeef2;
+          --color-prettylights-syntax-markup-ignored-bg: #0550ae;
+          --color-prettylights-syntax-meta-diff-range: #8250df;
+          --color-prettylights-syntax-brackethighlighter-angle: #57606a;
+          --color-prettylights-syntax-sublimelinter-gutter-mark: #8c959f;
+          --color-prettylights-syntax-constant-other-reference-link: #0a3069;
+
+          --color-fg-default: var( --rds-color-grey-2 );
+          --color-fg-muted: var( --rds-color-grey-4 );
+          --color-fg-subtle: var( --rds-color-grey-3 );
+          --color-canvas-default: var( --rds-color-grey-10 );
+          --color-canvas-subtle: var( --rds-color-grey-95 );
+          --color-border-default: var( --rds-color-grey-8 );
+          --color-border-muted: var( --rds-color-grey-6 );
+          --color-neutral-muted: var( --rds-color-grey-5 );
+          --color-accent-fg: var( --rds-color-primary-5 );
+          --color-accent-emphasis: var( --rds-color-primary-5 );
+          --color-attention-subtle: var( --rds-color-primary-9 );
+          --color-danger-fg: var( --rds-color-primary-9 );
+
+          --color-header: var( --rds-color-primary-5 );
+          --color-header-text: var( --rds-color-grey-10 );
         }
       }
     </style>
